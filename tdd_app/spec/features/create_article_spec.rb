173312203572676107b411f8da1542cfdd4a5bb7
nrkfeller <nrkfeller@gmail.com>
2015-11-26 02:40:09 +0000

    Added validations to article create is unsuccesful

--- tdd_app/spec/features/create_article_spec.rb
+++ tdd_app/spec/features/create_article_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature "Creating Articles" do
+   
+   ############ SCENARIO 1 ############ 
    scenario "User creates new articles" do
       visit "/" # root
       
@@ -15,5 +17,20 @@ RSpec.feature "Creating Articles" do
       expect(page.current_path).to eq(articles_path)
        
    end
-    
+   
+   ############ SCENARIO 2 ############ 
+   scenario "User fails to create new article" do
+      visit "/" # root
+      
+      click_link "New Article"
+      
+      fill_in "Title", with: ""
+      fill_in "Body", with: ""
+      click_button "Create Article"
+      
+      expect(page).to have_content("Article has not been created")
+      expect(page).to have_content("Title can't be blank")
+      expect(page).to have_content("Body can't be blank")
+   end
+      
 end
